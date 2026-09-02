930d0dd3ef2b564ab8f186bf3fb7278c4105bed6
Apex653 <gustav.merbom@gmail.com>
2026-09-02 10:31:54 +0200

    första uppgift i ptrogrammering

---
+++ 1_1.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace Uppgift_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); // raden skriver ut inom parentesen och byter rad
+            Console.WriteLine("Du har lärt dig skriva ut tecken som\" och \\ ");
+
+        }
+    }
+}
---
+++ 1_2.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            // Ändra brackgrundsfärg till blå
+            Console.ForegroundColor = ConsoleColor.White;
+            // Ändrar textfärgen till vit
+            Console.WriteLine("Fatal error!");
+            Console.WriteLine("Requirement need at least 512 GB DDR5");
+            Console.WriteLine("Go buy more!");
+            Console.WriteLine("If you want to use your computer again");
+            
+
+
+        }
+    }
+}
